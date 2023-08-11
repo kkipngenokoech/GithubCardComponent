6dc86fa723f6310b6dbd96d96f340807b8302b3b
kip <kkipngenokoech22@gmail.com>
2023-08-11 12:59:07 +0300

    the map function is happenning

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,39 +6,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const testDump = [
-    {name: "kkipngenokoech", avatar_url: "https://avatars.githubusercontent.com/u/84391547?v=4", bio: "He who threads softly goes far", company: "Kiota Academy", twitter_username: "Kkipngeno_koech"},
-    {
-		name: "kenjinote",
-		avatar_url: "https://avatars.githubusercontent.com/u/2605401?v=4",
-        bio: "dummy data",
-        company: "test",
-        twitter_username: "kenjinote"
-		
-	},
-	{
-		name: "gamemann",
-		avatar_url: "https://avatars.githubusercontent.com/u/6509565?v=4",
-		bio: "More dummy data",
-        company: "Game Mann",
-        twitter_username: "GameMann"
-	},
-	{
-		name: "karis2",
-		avatar_url: "https://avatars.githubusercontent.com/u/9477861?v=4",
-		bio: "fake bio",
-        company: "Karis two",
-        twitter_username: "GameKaris"
-	},
-]
 
 
 
 export default class GithubCard extends Component {
 
   render() {
-    const profile = testDump[0]
-    console.log(profile)
+    const profile  = this.props
     return (
       <Card style={{ maxWidth: 345 }} className='mx-auto m-4'>
         <CardMedia
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,42 @@
 import GithubCard from "./Card";
 
+const testDump = [
+    {name: "kkipngenokoech", avatar_url: "https://avatars.githubusercontent.com/u/84391547?v=4", bio: "He who threads softly goes far", company: "Kiota Academy", twitter_username: "Kkipngeno_koech"},
+    {
+		name: "kenjinote",
+		avatar_url: "https://avatars.githubusercontent.com/u/2605401?v=4",
+        bio: "dummy data",
+        company: "test",
+        twitter_username: "kenjinote"
+		
+	},
+	{
+		name: "gamemann",
+		avatar_url: "https://avatars.githubusercontent.com/u/6509565?v=4",
+		bio: "More dummy data",
+        company: "Game Mann",
+        twitter_username: "GameMann"
+	},
+	{
+		name: "karis2",
+		avatar_url: "https://avatars.githubusercontent.com/u/9477861?v=4",
+		bio: "fake bio",
+        company: "Karis two",
+        twitter_username: "GameKaris"
+	},
+]
+
+
+
+
 export default function CardList (props){
     return (
-
-    <div>
-        <GithubCard/>
+    <div className="container">
+        <div className="row">
+            {testDump.map((profile, id)=> {
+                return <GithubCard key={profile.id} {...profile}/>
+            })}
+        </div>
     </div>
     )
 }
